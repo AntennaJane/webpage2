16a02619186b8de9db393a034b5afa6b33c1c12d
KOremi(AntennaJane) <antennajane@yahoo.co.jp>
2020-03-20 20:18:57 +0900

    create index

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  document.title = "南次郎個人サーバ";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>南次郎個人サーバ</h1>
+      <p>
+        <a href="~Solferino/">AntennaJane実況本部</a><br/>
+        {/* "~Terminal/index.html" */}哲学者の日記帳（休止中）
+      </p>
     </div>
   );
 }
