be42b6c4fdc880c1706edce8f41af04074e50d3e
KOremi(AntennaJane) <antennajane@yahoo.co.jp>
2020-04-04 18:27:24 +0900

    fix App.test

--- src/pages/App.test.tsx
+++ src/pages/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {BrowserRouter} from "react-router-dom";
 import {render} from '@testing-library/react';
 import App from './App';
 
 test('renders index page', () => {
-  const {getByText} = render(<App/>);
+  const {getByText} = render(<BrowserRouter><App/></BrowserRouter>);
   const h1Element = getByText(/南次郎個人サーバ/i);
   expect(h1Element).toBeInTheDocument();
 });
